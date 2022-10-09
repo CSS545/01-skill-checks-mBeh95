be149b7443989ea9bbb88a30260d69a1539874f7
mBeh95 <melody.bdr7@gmail.com>
2022-10-09 13:19:36 -0700

    File Upload for Homework 1

---
+++ helloWorldButtons/app/src/main/java/com/example/helloworldbuttons/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.helloworldbuttons
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val clicking : Button= findViewById(R.id.buttonToNextPage);
+        clicking.setOnClickListener{
+            val moveToPageTwo = Intent(this, SecondPage::class.java);
+            startActivity(moveToPageTwo);
+
+        }
+    }
+}
